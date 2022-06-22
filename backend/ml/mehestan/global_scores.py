22017129e4b78cc2a78c91ca640e4828860d5fd1
cthiounn <thiounn.conrad@gmail.com>
2022-06-22 17:56:55 +0200

    WIP test

--- backend/ml/mehestan/global_scores.py
+++ backend/ml/mehestan/global_scores.py
@@ -368,7 +368,7 @@ def get_global_scores(scaled_individual_scores: pd.DataFrame, score_mode: ScoreM
         w = scores.voting_weight
         theta = scores.score
         delta = scores.uncertainty
-        print(W, w, theta, delta)
+        print(scores)
         rho = QrMed(2 * W, w, theta, delta)
         rho_uncertainty = QrUnc(2 * W, 1, w, theta, delta, qr_med=rho)
         rho_deviation = QrDev(2 * W, 1, w, theta, delta, qr_med=rho)
