3790bf74ab7e95afcfb7d9147db63b0dabec8b57
cthiounn <thiounn.conrad@gmail.com>
2022-06-23 08:11:20 +0200

    WIP test

--- backend/ml/mehestan/online_heuristics.py
+++ backend/ml/mehestan/online_heuristics.py
@@ -212,7 +212,10 @@ def _run_online_heuristics_for_criterion(
 
     for mode in ScoreMode:
         if mode == ScoreMode.DEFAULT:
-            global_scores = get_global_scores(partial_scaled_scores_for_ab, score_mode=mode)
+            print("mode", mode)
+            global_scores = get_global_scores(
+                partial_scaled_scores_for_ab, score_mode=mode
+            )
             global_scores["criteria"] = criteria
             save_entity_scores(
                 poll, global_scores, single_criteria=criteria, score_mode=mode
