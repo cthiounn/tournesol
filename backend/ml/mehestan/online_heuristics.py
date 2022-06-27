88f9c27dc996b1a5800b1c538ed9ac8fad4fac5f
cthiounn <thiounn.conrad@gmail.com>
2022-06-27 15:59:19 +0200

    test

--- backend/ml/mehestan/online_heuristics.py
+++ backend/ml/mehestan/online_heuristics.py
@@ -86,9 +86,12 @@ def get_new_scores_from_online_update(
         previous_individual_raw_scores.loc[id_entity_b] = 0.0
 
     dot_product = U_ab.dot(previous_individual_raw_scores)
-    theta_star_a = L_tilde_a - dot_product[dot_product.index == id_entity_a].values
-    theta_star_b = L_tilde_b - dot_product[dot_product.index == id_entity_b].values
-    print(theta_star_a.squeeze(), theta_star_b)
+    theta_star_a = (
+        L_tilde_a - dot_product[dot_product.index == id_entity_a].values
+    ).squeeze()
+    theta_star_b = (
+        L_tilde_b - dot_product[dot_product.index == id_entity_b].values
+    ).squeeze()
 
     previous_individual_raw_scores.loc[id_entity_a] = theta_star_a
     previous_individual_raw_scores.loc[id_entity_b] = theta_star_b
