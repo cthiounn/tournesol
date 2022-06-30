a85f496fce6c088b482feabe1909bc4366e462f1
cthiounn <thiounn.conrad@gmail.com>
2022-06-30 09:31:24 +0200

    test++

--- backend/tournesol/tests/test_api_comparison.py
+++ backend/tournesol/tests/test_api_comparison.py
@@ -1000,6 +1000,7 @@ class ComparisonWithMehestanTest(TransactionTestCase):
         self,
     ):
         call_command("ml_train")
+        print(ContributorRatingCriteriaScore.objects.all())
 
         self.assertEqual(ContributorRatingCriteriaScore.objects.count(), 4)
         self.assertEqual(
@@ -1026,8 +1027,10 @@ class ComparisonWithMehestanTest(TransactionTestCase):
         )
 
         self.assertEqual(resp.status_code, 201, resp.content)
+        print(ContributorRatingCriteriaScore.objects.all())
 
         call_command("ml_train")
+        print(ContributorRatingCriteriaScore.objects.all())
         # Individual scores related to the new comparison have been computed
         self.assertEqual(
             ContributorRatingCriteriaScore.objects.filter(
@@ -1176,7 +1179,6 @@ class ComparisonWithOnlineHeuristicMehestanTest(TransactionTestCase):
             ).count(),
             0,
         )
-
         self.client.force_authenticate(self.user5)
         resp = self.client.post(
             f"/users/me/comparisons/{self.poll.name}",
@@ -1273,6 +1275,58 @@ class ComparisonWithOnlineHeuristicMehestanTest(TransactionTestCase):
             EntityCriteriaScore.objects.filter(score_mode="default").count(), 2
         )
 
+    def test_delete_individual_scores_after_new_comparison_with_online_heuristic_update(
+        self,
+    ):
+        call_command("ml_train")
+        contrib_before_update = set(
+            ContributorRatingCriteriaScore.objects.all().values_list()
+        )
+        self.assertEqual(ContributorRatingCriteriaScore.objects.count(), 8)
+        self.assertEqual(
+            EntityCriteriaScore.objects.filter(score_mode="default").count(), 2
+        )
+
+        self.client.force_authenticate(self.user4)
+        resp = self.client.delete(
+            f"/users/me/comparisons/{self.poll.name}/{self.entities[0].uid}/{self.entities[1].uid}/",
+            data={
+                "criteria_scores": [{"criteria": "criteria1", "score": 10}],
+            },
+            format="json",
+        )
+
+        self.assertEqual(resp.status_code, 204, resp.content)
+
+        # WIP
+        self.assertEqual(
+            ContributorRatingCriteriaScore.objects.filter(
+                contributor_rating__user=self.user4
+            ).count(),
+            0,
+        )
+        # The score related to the less prefered entity is negative
+        user_score = ContributorRatingCriteriaScore.objects.get(
+            contributor_rating__user=self.user4,
+            contributor_rating__entity=self.entities[0],
+            criteria="criteria1",
+        )
+        self.assertLess(user_score.score, 0)
+
+        contrib_after_update = set(
+            ContributorRatingCriteriaScore.objects.all().values_list()
+        )
+
+        diff_update = contrib_after_update.difference(contrib_before_update)
+        # the update has generate two differences
+        self.assertEqual(len(diff_update), 2)
+        self.assertEqual(len(contrib_before_update.difference(contrib_after_update)), 2)
+        # no new individual scores 8+0=8
+        self.assertEqual(ContributorRatingCriteriaScore.objects.count(), 8)
+        # no new global scores = 2
+        self.assertEqual(
+            EntityCriteriaScore.objects.filter(score_mode="default").count(), 2
+        )
 
 class ComparisonApiWithInactivePoll(TestCase):
     def setUp(self):
